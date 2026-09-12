e1bad312870c3fc0758ee903dd0acc148c46475b
has-kas <hgkassim2026@gmail.com>
2026-09-12 01:02:28 +0100

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Paste the deployed Google Apps Script /exec URL here.
-// Example:
-// const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwExGdfxEs7n6pufvWqhJhFVgGwYUCm_oLKHnSwAWobiBCgprEg07iMAz7Azhqz9gpz/exec';
-const GAS_WEB_APP_URL = 'PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE';
